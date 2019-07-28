e4f1fccd788e6deb7d7fcbbcd6bc5a143b1bdb1e
nooblisk <mitarevigor@gmail.com>
2019-07-28 23:17:58 +0300

    Updates

---
+++ precache-manifest.b0c80a27c78681616ae70bc64813cfdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1da787aa9bbf7555b8c295c7b7cf9c6",
+    "url": "/mtp-react-frontend/index.html"
+  },
+  {
+    "revision": "a8d10c3a5453bfa7c3df",
+    "url": "/mtp-react-frontend/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "60af20a805150d7add6d",
+    "url": "/mtp-react-frontend/static/css/main.fe26f80c.chunk.css"
+  },
+  {
+    "revision": "a8d10c3a5453bfa7c3df",
+    "url": "/mtp-react-frontend/static/js/2.c664c455.chunk.js"
+  },
+  {
+    "revision": "60af20a805150d7add6d",
+    "url": "/mtp-react-frontend/static/js/main.c2e23988.chunk.js"
+  },
+  {
+    "revision": "bd9206a8c6309d5ce914",
+    "url": "/mtp-react-frontend/static/js/runtime~main.b1a8d6b9.js"
+  }
+]);
